5fdfbdddb51086df04f01b13220246a5e0b87a1e
Magda Dmitruk <dmitrukm@gmail.com>
2024-04-22 17:31:53 +0000

    create statefulgreeting with callback function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import StatefulGreeting from './components/StatefulGreeting';
+import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback'
 
 function App() {
   return (
     <div className="App">
-      <StatefulGreeting greeting="I am a stateful class component." name="Magda"/>
+      <StatefulGreetingWithCallback greeting="I am a stateful class component." name="Magda"/>
     </div>
   );
 }
--- my-app/src/components/StatefulGreetingWithCallback.js
+++ my-app/src/components/StatefulGreetingWithCallback.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-class StatefulGreeting extends React.Component {
+class StatefulGreetingWithCallback extends React.Component {
 
     constructor(props) {
         super(props);
@@ -15,8 +15,12 @@ class StatefulGreeting extends React.Component {
         this.setState({
             introduction: "Goodbye!",
             buttonText: "Enter"
+        }, ()=>{
+        console.log("new state ", this.state.introduction);
+        console.log("new state ", this.state.buttonText);
         });
         console.log(this.state.introduction);
+        console.log(this.state.buttonText);
     }
 
     render() {
@@ -30,4 +34,4 @@ class StatefulGreeting extends React.Component {
         
 }
 
-export default StatefulGreeting;
+export default StatefulGreetingWithCallback;
