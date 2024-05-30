4fa10faa52b9b6f570f778ae865ce6bac3a5542d
Magda Dmitruk <dmitrukm@gmail.com>
2024-05-30 14:20:40 +0000

    controlled form 2

--- my-app/src/components/ControlledForm.js
+++ my-app/src/components/ControlledForm.js
@@ -5,7 +5,9 @@ export class ControlledForm extends Component {
         super(props)
     
         this.state = {
-             name: ''
+             name: '',
+             category: 'website',
+             comment: ''
         }
     }
 
@@ -14,11 +16,25 @@ export class ControlledForm extends Component {
         name: event.target.value
       })
     }
+
+    handleCategoryChange = (event) => {
+      this.setState({
+        category: event.target.value
+      })
+    }
+
+    handleCommentsChange = (event) => {
+      this.setState({
+        comment: event.target.value
+      })
+    }
     
     render() {
         return (
             <div>
+                <h2>Please fill out the form below:</h2>
                 <form>
+                  <div>
                     <label htmlFor="id-name">Your Name:</label>
                     <input
                         value={this.state.name}
@@ -27,7 +43,29 @@ export class ControlledForm extends Component {
                         name="name"
                         type="text"
                     />
+                  </div>
+                  <div>
+                    <label htmlFor="id-category">Query Category:</label>
+                    <select 
+                      id="id-category" 
+                      name="category"
+                      value={this.state.category}
+                      onChange={this.handleCategoryChange}
+                    >
+                      <option value="website"> Website issue</option>
+                      <option value="order"> Order issue</option>
+                      <option value="general"> General inquiry</option>
+                    </select>
+                    <div>
+                      <label htmlFor='id-comments'>Comments:</label>
+                      <textarea 
+                        onChange={this.handleCommentsChange} 
+                        value={this.state.comment} 
+                        id="id-comments" 
+                        name="comments" />
+                    </div>
                     <input type="submit" value="Submit" />
+                  </div>
                 </form>
             </div>
         )
