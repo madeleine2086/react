8598dde88a21d3a16668e7ad55d97c6e72c09be1
Magda Dmitruk <dmitrukm@gmail.com>
2024-06-02 19:45:33 +0000

    useEffect

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-import UseStateWithObjects from "./components/UseStateWithObjects";
+import UseEffectCounter from "./components/UseEffectCounter";
+
 
 
 function App() {
   return <div className="App">
-      <UseStateWithObjects />
+      <UseEffectCounter />
     </div>;
 }
 
